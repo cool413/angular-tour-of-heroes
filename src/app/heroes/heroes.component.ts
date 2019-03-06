8e1afa28066e477a221db106d11fa5f3a661dc95
YouShengYuan <cool82413@gmail.com>
2019-03-06 17:56:58 +0800

    顯示英雄列表done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+
   heroes = HEROES;
   constructor() {}
 
   ngOnInit() {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
