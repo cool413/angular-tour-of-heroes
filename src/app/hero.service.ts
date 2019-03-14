b4d5df133935a52ff4d372b3e5e5661dde0bd034
cool413 <cool82413@gmail.com>
2019-03-15 00:53:52 +0800

    导航到英雄详情

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,7 +9,7 @@ import { MessagesService } from './messages.service';
 export class HeroService {
   constructor(private messageService: MessagesService) {}
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService:fetched heroes');
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
