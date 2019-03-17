caf0e117c3b47530baa9c92a8652912cf2a54f79
cool413 <cool82413@gmail.com>
2019-03-17 16:32:10 +0800

    HTTP

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,17 +8,13 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-
   heroes: Hero[];
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeros();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
   getHeros(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
