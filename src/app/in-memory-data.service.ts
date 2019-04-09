c4ec7c1943c601be216ba163649729bb193e1699
DerekYou <cool82413@gmail.com>
2019-04-09 23:27:09 +0800

    final

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -22,5 +22,16 @@ export class InMemoryDataService {
     return { heroes };
   }
 
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0
+      ? Math.max(...heroes.map(hero => hero.id)) + 1
+      : 11;
+  }
+
   constructor() {}
 }
